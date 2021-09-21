7f97a61a392c270aeafade5d23fb569dd63648c0
James Brownie <jamespbrownie@gmail.com>
2021-09-21 10:26:46 -0500

    commit tree 2b878cf85775c755cda98c2a3f44dbdc0a9bc741

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+function saturdayFun (i = "roller-skate") {
+    return `This Saturday, I want to ${i}!`
+}
+
+function mondayWork (i = "go to the office") {
+    return `This Monday, I will ${i}.` 
+}
+
+
+function wrapAdjective (z = "*") {
+    return function (i = 'special') {
+        return `You are ${z}${i}${z}!`
+    }
+}
